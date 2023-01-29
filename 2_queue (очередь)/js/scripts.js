02f0a9e572166d80598c1ae7ad02bbb859752254
baxromxoja16 <aglamof@mail.ru>
2023-01-29 20:38:02 +0500

    deleted some files

--- 2_queue (очередь)/js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(".item .ask").click(function () {
-  var $answ = $(this).parent().find(".answer");
-  //   var $answ = $(this).next();
-  
-  $(".item .ask").not(this).parent().find(".answer").slideUp();
-  
-  $answ.slideToggle();
-});
